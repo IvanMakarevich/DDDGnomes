8080310db4aa65591681c8936d4c38f00e0faa1d
Ivan Makarevich <qwer253@yandex.ru>
2012-02-18 18:52:20 +0300

    Хаотическая работа.

---
+++ Gnomes.Mvc3Site/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Gnomes.Domain;
+
+namespace Gnomes.Mvc3Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            using (GnomesContext context = new GnomesContext())
+            {
+                var t = context.Gnomes.ToList();
+            }
+            return View();
+        }
+    }
+}
---
+++ Gnomes.Mvc3Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gnomes.Mvc3Site.MvcApplication" Language="C#" %>
